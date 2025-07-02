726c8c09162261f0baa5af33eb6ee23785ab042e
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-07-02 08:55:11 -0500

    feat: :construction: Se sigue trabajando

---
+++ Exercises/procedimientos.sql
@@ -0,0 +1,5 @@
+-- Active: 1751458388058@@127.0.0.1@3307@examen_mysql
+
+SHOW TABLES;
+
+-- 1. Cree un procedimiento llamado `ps_registrar_cliente_unico`  que inserta un nuevo cliente si su correo no está registrado .
